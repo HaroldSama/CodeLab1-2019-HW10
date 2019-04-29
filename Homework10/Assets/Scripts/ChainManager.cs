2e4012e368113e70014b8bd05ca59722e2a801e0
HaroldSama <122599Run>
2019-04-29 04:04:22 -0400

    Align Function Added

---
+++ Homework10/Assets/Scripts/ChainManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//USAGE: put under a chain Loop to get data from it
+//[ExecuteInEditMode]
+public class ChainManager : MonoBehaviour
+{
+    public List<GameObject> nodes = new List<GameObject>();
+    public List<Vector3> radius = new List<Vector3>();
+    public List<Vector3> tangents = new List<Vector3>();
+    public Vector3 hand;
+    
+    void Awake()
+    {
+        foreach (Transform child in transform)
+        {
+            //Get the nodes
+            nodes.Add(child.gameObject);
+            
+            //Get the vectors that point from the center to the nodes
+            Vector3 radiu = child.position - transform.position;
+            radius.Add(radiu);
+            Debug.DrawRay(transform.position, radiu, Color.red, float.MaxValue);
+            
+            //Get the vectors that tangent with this chain loop
+            Vector3 tangent = Vector3.Cross(radiu, transform.forward);
+            tangents.Add(tangent);
+            Debug.DrawRay(child.position, tangent, Color.green, float.MaxValue);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Homework10/Assets/Scripts/MouseDragger.cs
+++ Homework10/Assets/Scripts/MouseDragger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseDragger : MonoBehaviour
+public class MouseDragger : ChainManager
 {
 
     public Vector3 mousePos;
@@ -20,8 +20,12 @@ public class MouseDragger : MonoBehaviour
         mousePos = Input.mousePosition;
         mousePos.z = 10;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        
+        hand = nodes[0].transform.position - transform.position;
+        Debug.DrawRay(transform.position, hand, Color.blue);
+        //print(hand);
                 
-        print(mousePos);
+        //print(mousePos);
     }
 
     private void OnMouseDown()
@@ -36,5 +40,29 @@ public class MouseDragger : MonoBehaviour
         reference = mousePos - transform.position;
         //print(Vector3.Angle(reference, mousePos - transform.position));
         
+    }
+
+    private void OnMouseUp()
+    {
+        Align();
+    }
+
+    void Align()
+    {
+        //Find the closest radius to align by comparing every dot product between the radius and the hand     
+        List<float> comparon = new List<float>();
+        
+        foreach (var radiu in radius)
+        {
+            comparon.Add(Vector3.Dot(radiu, hand));
+            //print("radius" + radiu);
+            //print(Vector3.Dot(radiu, hand));
+        }
+
+        Vector3 target = radius[comparon.IndexOf(Mathf.Max(comparon.ToArray()))];
+        //print("hand" + hand);
+        //print("target" + target);
+
+        transform.Rotate(0, 0, Vector3.SignedAngle(hand, target, Vector3.forward));
     }
 }
